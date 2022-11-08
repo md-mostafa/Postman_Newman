99b5d0328d12392ad3ebc90bb262f3baf2dae30d
akash <md.mostafa.akash@gmail.com>
2022-11-08 18:45:31 +0600

    simple api test automation

---
+++ TestApiAutomation/src/runner/runner.js
@@ -0,0 +1,40 @@
+const newman = require('newman');
+
+
+
+newman.run({
+    collection:'./src/collection/TestApiAutomation.postman_collection.json',
+    reporters: ['htmlextra'],
+    iterationCount: 1,
+    reporter: {
+        htmlextra: {
+             //export: baseReport+'/api_report.html',
+            export: './report/api_report.html',
+            // template: './template.hbs'
+            // logs: true,
+            // showOnlyFails: true,
+            // noSyntaxHighlighting: true,
+            // testPaging: true,
+            // browserTitle: "My Newman report",
+            // title: "My Newman Report",
+            // titleSize: 4,
+            // omitHeaders: true,
+            // skipHeaders: "Authorization",
+            // omitRequestBodies: true,
+            // omitResponseBodies: true,
+            // hideRequestBody: ["Login"],
+            // hideResponseBody: ["Auth Request"],
+            // showEnvironmentData: true,
+            // skipEnvironmentVars: ["API_KEY"],
+            // showGlobalData: true,
+            // skipGlobalVars: ["API_TOKEN"],
+            // skipSensitiveData: true,
+            // showMarkdownLinks: true,
+            // showFolderDescription: true,
+            // timezone: "Australia/Sydney",
+            // skipFolders: "folder name with space,folderWithoutSpace",
+            // skipRequests: "request name with space,requestNameWithoutSpace",
+            // displayProgressBar: true
+        }
+    }
+});
